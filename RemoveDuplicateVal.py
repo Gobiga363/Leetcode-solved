7fbedfbd648c3a09d4cafb3ee01f32ad48b7392d
Gobiga <110178279+Gobiga363@users.noreply.github.com>
2024-02-24 22:48:58 +0530

    #26

---
+++ RemoveDuplicateVal.py
@@ -0,0 +1,20 @@
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        l = 1
+        for r in range(1, len(nums)):
+            if nums[r] != nums[r - 1]:
+                nums[l] =nums[r]
+                l += 1
+        return l
+
+"""
+Example 1:
+
+Input: nums = [1,1,2]
+Output: 2, nums = [1,2,_]
+
+Example 2:
+
+Input: nums = [0,0,1,1,1,2,2,3,3,4]
+Output: 5, nums = [0,1,2,3,4,_,_,_,_,_]
+"""
