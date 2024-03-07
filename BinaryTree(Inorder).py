5c9fe085dd7e723d204c14a9e888b9c16b1af905
Gobiga <110178279+Gobiga363@users.noreply.github.com>
2024-03-07 22:52:39 +0530

    #94

---
+++ BinaryTree(Inorder).py
@@ -0,0 +1,32 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        result = []
+
+        def inorder(root):
+            if not root:
+                return
+            inorder(root.left)
+            result.append(root.val)
+            inorder(root.right)
+        inorder(root)
+        return result
+
+"""
+Example 1
+Input: root = [1,null,2,3]
+Output: [1,3,2]
+
+Example 2:
+Input: root = []
+Output: []
+
+Example 3:
+Input: root = [1]
+Output: [1]
+"""
