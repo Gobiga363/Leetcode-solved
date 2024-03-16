2796d33f8e84245d65263e2ea73a311e173ca171
Gobiga <110178279+Gobiga363@users.noreply.github.com>
2024-03-16 23:24:55 +0530

    #112

---
+++ PathSum.py
@@ -0,0 +1,33 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
+        if not root:
+            return False
+
+        if not root.left and not root.right:
+            return targetSum == root.val
+
+        left_sum = self.hasPathSum(root.left, targetSum - root.val)
+        right_sum = self.hasPathSum(root.right, targetSum - root.val)
+
+        return left_sum or right_sum
+
+
+"""
+Example 1:
+Input: root = [5,4,8,11,null,13,4,7,2,null,null,null,1], targetSum = 22
+Output: true
+
+Example 2:
+Input: root = [1,2,3], targetSum = 5
+Output: false
+
+Example 3:
+Input: root = [], targetSum = 0
+Output: false
+"""
